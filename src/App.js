798ffeb485a9b1ae91e7a52755cd1d2934e88732
roydbaha <rdvbahamut@gmail.com>
2021-01-02 20:50:10 -0600

    Proyecto Inicial

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+ import styled from '@emotion/styled';
+
+// const Boton = styled.button`
+// background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+// background-size: 300px;
+// font-family:  Arial, Helvetica, sans-serif;
+// color: #fff;
+// margin-top: 3rem;
+// padding: 1rem 3rem;
+// font-size: 2rem;
+// border: 2px solid black;
+// ` ;
+
+
+function App() {
+  return (
+   <div>h</div>
+  );
+}
+
+export default App;
